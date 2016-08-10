cb95c96ff6a15db3ecfa70f8ed824066982dde9b
Tyler Benton <tjbenton21@gmail.com>
2016-08-10 12:34:36 -0400

    reworked the main docs function
    This update improves the readability as well as functionality. It improves the speed things are run and decreases the complexity.

--- packages/docs-parser/src/docs-parser.js
+++ packages/docs-parser/src/docs-parser.js
@@ -34,7 +34,7 @@ async function docsParser(options = {}, callback) {
   /* eslint-disable no-unused-vars */
   // these are all the options that can be used
   let {
-    files: initial_files,
+    files: globs,
     ignore,
     blank_lines,
     page_fallback,
@@ -56,105 +56,83 @@ async function docsParser(options = {}, callback) {
 
   let json = {}
   let parsers = {}
-
+  const parser_options = { page_fallback, blank_lines, indent, annotations, sort, log: logger }
   const root = process.cwd()
+  const watcher = chokidar.watch(globs, { persistent: true, ignoreInitial: true })
 
-  let walk = async (files) => {
-    files = to.array(files)
-
-    logger.emit('start', 'total')
+  const parse = async (file, should_log = false) => {
     try {
-      logger.emit('start', 'paths')
-      files = await globby(files, { ignore, nodir: true })
-
-      let paths_message = `%s completed ${to.map(files, (file) => clor.bold(file.replace(process.cwd() + '/', ''))).join(', ')} after %dms`
-      if (files.length > 3) {
-        let s = files.length > 1 ? 's' : '' // eslint-disable-line
-        paths_message = `%s completed after %dms with ${files.length} file${s} to parse`
-      }
-      logger.emit('complete', 'paths', paths_message)
-
-      logger.emit('start', 'parser')
-      options.annotationsApi = annotations
-
-      const parse = async ({ file, type }) => {
-        return {
-          [file]: await parsers[type].parse(file)
-        }
+      should_log && logger.emit('start', 'parser')
+      const ext = path.extname(file).replace('.', '')
+      let parser = parsers[ext]
+      file = file.replace(`${root}${path.sep}`, '')
+      if (!parser) {
+        parser = parsers[ext] = new Parser(languages[ext] || languages.default, ext, parser_options)
       }
 
-
-      const parser_options = { page_fallback, blank_lines, indent, annotations, sort, log: logger }
-      const parsed_files = await map(files, (file) => {
-        const type = path.extname(file).replace('.', '')
-        if (!parsers[type]) {
-          parsers[type] = new Parser(languages[type] || languages.default, type, parser_options)
-        }
-
-        return parse({ file, type })
-      })
-
-      logger.emit('complete', 'parser')
-
-      // Loop through the parsed files and update the
-      // json data that was stored.
-      for (let file of parsed_files) {
-        to.extend(json, file)
-      }
-
-      let result = json
-
-      if (!raw) {
-        logger.emit('start', 'sorter')
-        result = sorter({ json, page_fallback, log: logger })
-        logger.emit('complete', 'sorter')
-      }
-
-      logger.emit('complete', 'total')
-      timestamps && logger.space()
-
-      logger.emit('parsed', result, files)
-
-      if (typeof callback === 'function') {
-        callback(result, files)
-      }
-
-      return result
+      json[file] = await parser.parse(file)
+      should_log && logger.emit('complete', 'parser')
     } catch (err) {
-      logger.error(err.stack)
+      logger.emit('error', file, 'was not updated', err)
     }
   }
 
+  const sortdata = () => {
+    if (raw) {
+      return json
+    }
+    logger.emit('start', 'sorter')
+    const result = sorter({ json, page_fallback, log: logger })
+    logger.emit('complete', 'sorter')
+    return result
+  }
 
-  let result = await walk(initial_files)
+  const parsefile = async (file) => {
+    logger.emit('start', 'total')
+    await parse(file, true)
+    const result = sortdata()
+    logger.emit('complete', 'total')
+    logger.emit('parsed', result, [ file ])
+
+    if (typeof callback === 'function') {
+      callback(result, [ file ])
+    }
+
+    return result
+  }
+
+  const parsefiles = async (_globs) => {
+    logger.emit('start', 'total')
+    let files = await map(_globs, (glob) => globby(glob, { ignore, nodir: true }))
+    files = to.flatten(files)
+    logger.emit('start', 'parser')
+    await map(files, parse)
+    logger.emit('complete', 'parser')
+    const result = sortdata()
+    logger.emit('complete', 'total')
+    logger.emit('parsed', result, files)
+
+    if (typeof callback === 'function') {
+      callback(result, files)
+    }
+
+    return result
+  }
+
+  const result = await parsefiles(globs)
 
   if (!watch) {
     return result
   }
 
-  initial_files = await globby(initial_files, { ignore, nodir: true })
-  initial_files = initial_files.map((file) => !path.isAbsolute(file) ? path.join(root, file) : file)
-
-  let watcher = chokidar.watch(initial_files, { persistent: true, ignoreInitial: true })
-
   logger.space()
-  logger.print('Watching', to.map(initial_files, (file) => clor.bold(file.replace(`${root}/`, ''))).join(', '))
-  logger.print('Excluding', to.map(ignore, (file) => clor.bold(file.replace(`${root}/`, ''))).join(', '))
+  logger.print('Watching', to.map(globs, (file) => clor.bold(file)).join(', '))
+  logger.print('Excluding', to.map(ignore, (file) => clor.bold(file)).join(', '))
   logger.space()
 
-  watcher.on('all', async (type, file) => {
-    if (
-      type === 'add' ||
-      type === 'changed' ||
-      type === 'change'
-    ) {
-      try {
-        await walk(file)
-      } catch (err) {
-        logger.emit('error', file, 'was not updated', err)
-      }
-    }
-  })
+  watcher
+    .on('add', parsefile)
+    .on('change', parsefile)
 
   return watcher
 }
