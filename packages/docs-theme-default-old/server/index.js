d760f3ad8609b00eb171ce79eb208dbc342781a1
Tyler Benton <tjbenton21@gmail.com>
2016-09-27 13:19:55 -0400

    removed old files since they're no longer needed

--- packages/docs-theme-default-old/server/index.js
+++
@@ -1,7 +0,0 @@
-require('babel-register')({
-  presets: [ 'es2015', 'stage-0' ],
-  plugins: [ 'transform-runtime' ]
-})
-
-
-module.exports = require('./app.js').default
--- packages/docs-theme-default-old/server/routes/search.js
+++
@@ -1,5 +0,0 @@
-export default function search(app, router, controllers) {
-  app.get('/search', controllers.search.index)
-  app.get('/s', controllers.search.index)
-  return router
-}
