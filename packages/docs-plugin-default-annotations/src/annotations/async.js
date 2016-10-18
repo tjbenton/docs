316ce69329a76a7b57942ee5293cfe6d47bda413
Tyler Benton <tjbenton21@gmail.com>
2016-10-18 17:05:06 -0400

    added `@async`

---
+++ packages/docs-plugin-default-annotations/src/annotations/async.js
@@ -0,0 +1,16 @@
+/// @name @async
+/// @page annotations
+/// @description Used to notate that a function is chainable
+/// @returns {boolean}
+/// @markup Usage
+/// // this will return true
+/// /// @chainable
+///
+/// /// @chainable true
+///
+/// /// @chainable false
+export default {
+  parse() {
+    return true
+  }
+}
